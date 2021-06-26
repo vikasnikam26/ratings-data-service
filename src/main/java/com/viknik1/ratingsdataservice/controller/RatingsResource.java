8bad61903fea98424f1c28e65927b4d76c355bad
vikasnikam26 <vikas.nikam26@gmail.com>
2021-06-27 00:32:29 +0530

    added new Ratings class

--- src/main/java/com/viknik1/ratingsdataservice/controller/RatingsResource.java
+++ src/main/java/com/viknik1/ratingsdataservice/controller/RatingsResource.java
@@ -1,11 +1,32 @@
 package com.viknik1.ratingsdataservice.controller;
 
+import com.viknik1.ratingsdataservice.model.Rating;
+import com.viknik1.ratingsdataservice.model.UserRating;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingsResource {
-    
+
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId,4);
+    }
+
+    @RequestMapping("users/{userId}")
+    public UserRating getRatings(@PathVariable("userId") String userId) {
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234",3),
+                new Rating("4454",4)
+        );
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratings);
+        return userRating;
+    }
 }
 
